82e598df9cdc768cd272f9689483d2e8a80af2af
David Paul Graham <dpgraham4401@gmail.com>
2023-08-14 10:10:38 -0400

    use tfvars for varialbes in global scope

--- global/variables.tf
+++ global/variables.tf
@@ -1,12 +1,10 @@
 variable "billing_account" {
   description = "The ID of the billing account to associate projects with"
   type        = string
-  default     = "017B63-86F4B4-FB58DB"
 }
 
 variable "org_id" {
   description = "The organization id for the associated resources"
   type        = string
-  default     = "898217214682"
 }
 
